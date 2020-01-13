0a99690873bc05835207ffc81eb7a1d197e909db
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-01-13 14:21:14 +0100

    feat(): Display name of the marker and add highlight style on hover. Add comments

--- src/source.js
+++ src/source.js
@@ -12,31 +12,55 @@ import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import {OSM, Vector as VectorSource} from 'ol/source';
 import GeoJSON from 'ol/format/GeoJSON';
 import {transform} from 'ol/proj';
-import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style';
 
-// coordonnées récupérées depuis https://www.latlong.net/convert-address-to-lat-long.html
+// Coordonnées Seine Innopolis
 var nws = transform([1.06653, 49.42847], 'EPSG:4326', 'EPSG:3857');
+// Coordonnées des copeaux numériques
 var copeauxNumeriques = transform(
   [1.06467, 49.42222],
   'EPSG:4326',
   'EPSG:3857',
 );
+// Coordonnées de l'ISD
 var ISDFlaubert = transform([1.12029, 49.45093], 'EPSG:4326', 'EPSG:3857');
 
+// On definit la forme du marqueur
 var image = new CircleStyle({
   radius: 5,
   fill: null,
   stroke: new Stroke({color: 'red', width: 10}),
 });
 
+// Style du marqueur
 var styles = {
   Point: new Style({
     image: image,
   }),
 };
 
+// Style au survol d'un marqueur
+var highlightStyle = feature =>
+  new Style({
+    text: new Text({
+      text: selected.get('name'),
+      offsetY: -25,
+      scale: 1.5,
+    }),
+    image: new CircleStyle({
+      radius: 5,
+      text: 'toto',
+      fill: null,
+      stroke: new Stroke({
+        color: 'green',
+        width: 20,
+      }),
+    }),
+  });
+
 const styleFunction = feature => styles[feature.getGeometry().getType()];
 
+// GeoJSON Object
 var geojsonObject = {
   type: 'FeatureCollection',
   crs: {
@@ -48,6 +72,9 @@ var geojsonObject = {
   features: [
     {
       type: 'Feature',
+      properties: {
+        name: 'Normandie Web School',
+      },
       geometry: {
         type: 'Point',
         coordinates: nws,
@@ -55,6 +82,9 @@ var geojsonObject = {
     },
     {
       type: 'Feature',
+      properties: {
+        name: 'Les copeaux numériques',
+      },
       geometry: {
         type: 'Point',
         coordinates: copeauxNumeriques,
@@ -62,6 +92,9 @@ var geojsonObject = {
     },
     {
       type: 'Feature',
+      properties: {
+        name: 'ISD Flaubert',
+      },
       geometry: {
         type: 'Point',
         coordinates: ISDFlaubert,
@@ -90,6 +123,24 @@ var map = new Map({
   target: 'carteNWS',
   view: new View({
     center: nws,
-    zoom: 14,
+    zoom: 12,
   }),
+});
+
+// Marqueur survolé
+var selected = null;
+var status = document.getElementById('status');
+
+// Au survol d'un marqueur
+map.on('pointermove', event => {
+  if (selected !== null) {
+    selected.setStyle(undefined);
+    selected = null;
+  }
+
+  map.forEachFeatureAtPixel(event.pixel, feature => {
+    selected = feature;
+    feature.setStyle(highlightStyle(feature));
+    return true;
+  });
 });
