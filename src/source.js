a3c83fc110cb6c3069916c691942919fb8279d98
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-01-13 11:47:35 +0100

    feat(): Displaying 3 points on the map.

--- src/source.js
+++ src/source.js
@@ -2,27 +2,94 @@ import './source.scss';
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import Feature from 'ol/Feature';
+import {
+  defaults as defaultInteractions,
+  DragRotateAndZoom,
+} from 'ol/interaction';
+import Circle from 'ol/geom/Circle';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import GeoJSON from 'ol/format/GeoJSON';
 import {transform} from 'ol/proj';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
 
 // coordonnées récupérées depuis https://www.latlong.net/convert-address-to-lat-long.html
-var nws = transform([1.066530, 49.428470], 'EPSG:4326', 'EPSG:3857');
+var nws = transform([1.06653, 49.42847], 'EPSG:4326', 'EPSG:3857');
+var copeauxNumeriques = transform(
+  [1.06467, 49.42222],
+  'EPSG:4326',
+  'EPSG:3857',
+);
+var ISDFlaubert = transform([1.12029, 49.45093], 'EPSG:4326', 'EPSG:3857');
+
+var image = new CircleStyle({
+  radius: 5,
+  fill: null,
+  stroke: new Stroke({color: 'red', width: 10}),
+});
+
+var styles = {
+  Point: new Style({
+    image: image,
+  }),
+};
+
+const styleFunction = feature => styles[feature.getGeometry().getType()];
+
+var geojsonObject = {
+  type: 'FeatureCollection',
+  crs: {
+    type: 'name',
+    properties: {
+      name: 'EPSG:3857',
+    },
+  },
+  features: [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: nws,
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: copeauxNumeriques,
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: ISDFlaubert,
+      },
+    },
+  ],
+};
+
+var vectorSource = new VectorSource({
+  features: new GeoJSON().readFeatures(geojsonObject),
+});
+
+var vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style: styleFunction,
+});
 
 var map = new Map({
-  interactions: defaultInteractions().extend([
-    new DragRotateAndZoom()
-  ]),
+  interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
+    vectorLayer,
   ],
   target: 'carteNWS',
   view: new View({
-    projection: 'EPSG:3857',
     center: nws,
-    zoom: 14
-  })
+    zoom: 14,
+  }),
 });
